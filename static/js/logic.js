2b54113379662d2b238974fdcb577f3a6f721db0
[InaLuo2022] <117559158+InaLuo2022@users.noreply.github.com>
2023-03-12 18:30:06 +1100

    updated

--- static/js/logic.js
+++ static/js/logic.js
@@ -12,7 +12,6 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 // Use this link to get the GeoJSON data.
 let link = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson";
 
-
 // Creating a new marker:
 // We pass in some initial options, and then add the marker to the map by using the addTo() method.
 d3.json(link).then(function(data) {
@@ -22,40 +21,61 @@ d3.json(link).then(function(data) {
         let earthquake_lan = data.features[i].geometry.coordinates[1];
         let earthquake_lon = data.features[i].geometry.coordinates[0];
         let earthquake_mag = data.features[i].properties.mag;
+        let earthquake_depth = data.features[i].geometry.coordinates[2]
 
-        //let circle_color = [];
         let circle_fillColor = [];
         let circle_radius = 30000 * earthquake_mag;
 
-        if (earthquake_mag < 2.5) {
-            //circle_color = 'dark';
-            circle_fillColor = '#ecf542';
+        if (earthquake_depth <= 10 & earthquake_depth > -10) {
+            circle_fillColor = '#FEB24C';
             }
-        else if (earthquake_mag < 5.4 & earthquake_mag >=2.5 ) {
-            //circle_color = '#2eab51';
-            circle_fillColor = '#2eab51';
+        else if (earthquake_depth <= 30 & earthquake_depth > 10) {
+            circle_fillColor = '#FD8D3C';
             }
-        else if (earthquake_mag < 6.0 & earthquake_mag >=5.5 ) {
-            //circle_color = '#101078';
-            circle_fillColor = '#101078';
+        else if (earthquake_depth <= 50 & earthquake_depth > 30) {
+            circle_fillColor = '#FC4E2A';
             }
-        else if (earthquake_mag < 6.9 & earthquake_mag >=6.1 ) {
-            //circle_color = '#570c12';
-            circle_fillColor = '#570c12';
+        else if (earthquake_depth <= 70 & earthquake_depth > 50) {
+            circle_fillColor = '#E31A1C';
             }
-        else if (earthquake_mag >= 7.0 ) {
-            //circle_color = '#0d0101';
-            circle_fillColor = '#0d0101';
+        else if (earthquake_depth <= 90 & earthquake_depth > 70) {
+            circle_fillColor = '#BD0026';
             }
+        else if (earthquake_depth > 90) {
+            circle_fillColor = '#7a0177';
+        };
 
         let circles = L.circle([earthquake_lan, earthquake_lon], {
             color: circle_fillColor,
+            weight: 2,
             fillColor: circle_fillColor,
-            fillOpacity: 1,
+            fillOpacity: 0.8,
             radius: circle_radius,
         }).addTo(EarthQuakeMap);
 
-        circles.bindPopup("Earthquake Magnitude:" + earthquake_mag);
-    };
+        let paragraphs = '<p>' + "Magnitude: " + earthquake_mag +'</p>'
+                + '<p>'+ "Coordinates: " + earthquake_lan + "," + earthquake_lon + '</p>'
+                + '<p>'+ "Earthquake Depth: " + earthquake_depth + "km" + '</p>';
 
+        circles.bindPopup(paragraphs);
+    };
 });
+
+// create legend
+// var getColor = ["#FEB24C", "#FD8D3C", "#FC4E2A", "#E31A1C", "#BD0026","#7a0177"];
+
+var legend = L.control({position: 'bottomright'});
+legend.onAdd = function (map) {
+    var div = L.DomUtil.create('div', 'legend');
+    div.innerHTML += "Earthquake<br>";
+    div.innerHTML += "Depth(km)<br>";
+    div.innerHTML += '<i style="background: #FEB24C"></i><span>-10-10</span><br>';
+    div.innerHTML += '<i style="background: #FD8D3C"></i><span>10-30</span><br>';
+    div.innerHTML += '<i style="background: #FC4E2A"></i><span>30-50</span><br>';
+    div.innerHTML += '<i style="background: #E31A1C"></i><span>50-70</span><br>';
+    div.innerHTML += '<i style="background: #BD0026"></i><span>70-90</span><br>';
+    div.innerHTML += '<i style="background: #7a0177"></i><span>90+</span><br>'
+    return div;
+};
+
+legend.addTo(EarthQuakeMap)
